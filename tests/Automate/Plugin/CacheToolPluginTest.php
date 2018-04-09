f11acc05b01f12cdc1229116f9c0918cc76f4a4a
Romaric Paul <romaric.paul@businessdecision.com>
2018-04-09 11:04:57 +0200

    test cache tools OK

--- tests/Automate/Plugin/CacheToolPluginTest.php
+++ tests/Automate/Plugin/CacheToolPluginTest.php
@@ -19,6 +19,7 @@ use Automate\Plugin\GitterPlugin;
 use Automate\Session\SessionInterface;
 use Automate\Tests\AbstractContextTest;
 use Phake;
+use phpseclib\Net\SSH2;
 
 class CacheToolPluginTest extends AbstractContextTest
 {
@@ -28,6 +29,32 @@ class CacheToolPluginTest extends AbstractContextTest
         $context = $this->createContext(Phake::mock(SessionInterface::class), Phake::mock(LoggerInterface::class));
         $cacheTool->register($context->getProject());
 
-        //Phake::verify($cacheTool, Phake::times(0))->sendMessage();
+        Phake::verify($cacheTool, Phake::times(0))->onTerminate();
+    }
+
+    public function testSimpleConfig()
+    {
+        $cacheTool = new CacheToolPlugin();
+        $session  = Phake::mock(SessionInterface::class);
+        $context = $this->createContext($session, Phake::mock(LoggerInterface::class));
+
+        $context->getProject()->setPlugins(['cache_tool' => [
+            'opcache' => 'true',
+            'apcu' => 'true',
+            'apc' => 'true'
+        ]]);
+
+        $cacheTool->register($context->getProject());
+        $cacheTool->onTerminate(new DeployEvent($context));
+
+        $path = $context->getReleasePath(current($context->getProject()->getPlatform('development')->getServers()));
+
+        Phake::inOrder(
+            Phake::verify($session, Phake::times(1))->run('cd '.$path.'; curl -sO ' . CacheToolPlugin::PHAR_URL),
+            Phake::verify($session, Phake::times(1))->run('cd '.$path.'; php cachetool.phar opcache:reset --fcgi'),
+            Phake::verify($session, Phake::times(1))->run('cd '.$path.'; php cachetool.phar apcu:cache:clear --fcgi'),
+            Phake::verify($session, Phake::times(1))->run('cd '.$path.'; php cachetool.phar apc:cache:clear --fcgi'),
+            Phake::verify($session, Phake::times(1))->run('cd '.$path.'; rm cachetool.phar')
+        );
     }
 }
