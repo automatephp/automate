fc523e2f6511322ab6d26b17db343faada3651cf
Romaric Paul <romaric.paul@businessdecision.com>
2018-06-12 12:01:34 +0200

    init teams microsoft

---
+++ src/Automate/Plugin/TeamPlugin.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * This file is part of the Automate package.
+ *
+ * (c) Julien Jacottet <jjacottet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Automate\Plugin;
+
+/**
+ * Allow to send a notification to your channel Slack
+ * if the deployment is success or failed
+ * only if you're deploying from your remote (not from gitlab)
+ *
+ * @author Julien Jacottet <jjacottet@gmail.com>
+ * @author Romaric Paul <romaric.paul@gmail.com>
+ *
+ */
+
+
+class TeamPlugin extends AbstractNotificationPlugin
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'team';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigurationSchema()
+    {
+        return [
+            '_type' => 'array',
+            '_children' => [
+                'hook_uri' => [
+                    '_type' => 'text',
+                    '_required' => true,
+                    '_not_empty' => true,
+                ],
+                'messages' => $this->getMessagesSchema()
+            ]
+        ];
+    }
+
+    /**
+     * @param string $message
+     */
+    protected function sendMessage($message, $eventName)
+    {
+        $this->client->request(
+            'POST', $this->configuration['hook_uri'],
+            [
+                'json' => [
+                    'text' => $message
+                ],
+                'verify' => false
+            ]
+        );
+    }
+}
